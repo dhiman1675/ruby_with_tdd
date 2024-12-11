ec656d4400ccac119a97444269b2ebcff319fddc
ROHIT <dhiman1675@gmail.com>
2024-12-12 00:08:56 +0530

    test cases: new lines + delimiter + new line after customer delimter + exception

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -22,6 +22,23 @@ describe StringCalculator do
     it 'returns the sum of multiple numbers' do
       expect(calculator.add('1,2,3,4')).to eq(10)
     end
+
+    it 'handles new lines as a delimiter' do
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports custom delimiters' do
+      expect(calculator.add("//;\n1;2;3")).to eq(6)
+    end
+
+    it 'handles new lines after supporting custom delimiters' do
+      expect(calculator.add("//;\n1;2\n3")).to eq(6)
+    end
+
+    it 'raises an exception for negative numbers' do
+      expect { calculator.add('1,-2,3,-4') }
+        .to raise_error('negative numbers not allowed: -2, -4')
+    end
   end
 end
 
